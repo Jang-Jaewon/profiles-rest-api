a5ee7176e6f605c2d00d5deaf3928bd4dbe3c482
Jang-Jaewon <jewon119@gmail.com>
2022-06-04 15:18:39 +0900

    Fixed: AbstractUser -> AbstractBaseUser

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
@@ -30,16 +31,16 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-class UserProfile(AbstractUser, PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
-    email = models.EmailField(max_length=256, unique=True)
-    name = models.CharField(max_length=256)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
@@ -47,7 +48,7 @@ class UserProfile(AbstractUser, PermissionsMixin):
         return self.name
 
     def get_short_name(self):
-        """Retrieve short name of user"""
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
--- profile_api/views.py
+++ profile_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profile_api import serializers
-from .models import UserProfile
+from profile_api import models
 
 
 class HelloAPIView(APIView):
@@ -84,6 +84,10 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({"http_method": "DELETE"})
 
 
+class UserPRofileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
 
 
 
