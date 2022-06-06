b22dcab43d22db88f8d95e11abc356abaae0d89d
Jang-Jaewon <jewon119@gmail.com>
2022-06-06 14:47:48 +0900

    Add: IsAuthenticatedOrReadonly and UpdateOwnstatus permission

--- profile_api/views.py
+++ profile_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.settings import api_settings
 from rest_framework import filters
 
@@ -109,6 +110,10 @@ class UserLoginAPIView(ObtainAuthToken):
 class UserProfileFeedViewset(viewsets.ModelViewSet):
     """Handle creating, reading and updating profile fedd items"""
     authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly,
+    )
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
 
